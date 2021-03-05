59e8b94edca71f5435d9ae7ac82c81fd694a5538
unknown <hari21032001@gmail.com>
2021-03-05 23:06:49 +0530

    Pdf fetching is added

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import controlstyles from '../Css/Controls.module.css';
 
-function Controls() {
+function Controls(props) {
+
+    const{numPages,pageNumber,setPageNumber}=props;
+
+
+    const islastpage=pageNumber!=numPages?"":"disabled";
+    const nextpage=()=>{
+        if(numPages!=pageNumber)setPageNumber(pageNumber+1);
+         console.log(islastpage);
+    }
+    const previouspage=()=>{
+        setPageNumber(pageNumber-1);
+    }
+    const firstpage=()=>{
+       setPageNumber(1);
+    }
+    const lastpage=()=>{
+        if(numPages!=pageNumber) setPageNumber(numPages);
+    }
+
+
+    const isfirstpage=pageNumber===1?"disabled":"";
+    
+
     return (
         <div style={{display:"flex",flexWrap:"wrap",width:"100%", background:"#f50057",borderRadius:"5px",height:"50px",color:"white",alignItems:"center",justifyContent:"center"}}>
            
         <div className={controlstyles.page_controls}>
             
-            <button className="btn-floating waves-effect waves-light pink darken-4"><i class="fa fa-fast-backward " aria-hidden="true"></i></button>
-            <button className="btn-floating waves-effect waves-light pink darken-4"><i class="fa fa-backward " aria-hidden="true"></i></button>
-            <input type="number" style={{width:"40px",border:"1px solid white",height:"20px",color:"white",background:"pink",margin:"0"}}></input><h6>/ 24</h6>
-             <button className="btn-floating waves-effect waves-light pink darken-4"><i class="fa fa-forward " aria-hidden="true"></i></button>
-             <button className="btn-floating waves-effect waves-light pink darken-4"><i class="fa fa-fast-forward " aria-hidden="true"></i></button>
+            <button  className={`btn-floating waves-effect waves-light pink darken-4 ${isfirstpage}`} onClick={firstpage}><i class="fa fa-fast-backward " aria-hidden="true"></i></button>
+            <button className={`btn-floating waves-effect waves-light pink darken-4 ${isfirstpage}`}onClick={previouspage}><i class="fa fa-backward " aria-hidden="true"></i></button>
+            <input type="number" style={{width:"40px",border:"1px solid white",height:"20px",color:"white",background:"pink",margin:"0"}} value={pageNumber}></input><h6>/ {numPages}</h6>
+             <button className={`btn-floating waves-effect waves-light pink darken-4 ${islastpage}`} onClick={nextpage}  ><i class="fa fa-forward " aria-hidden="true"></i></button>
+             <button className={`btn-floating waves-effect waves-light pink darken-4 ${islastpage}`} onClick={lastpage} ><i class="fa fa-fast-forward " aria-hidden="true"></i></button>
            
             </div>
            
--- src/Components/Pdfreader.js
+++ src/Components/Pdfreader.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import {Document,Page,pdfjs} from 'react-pdf';
+import {Document,Page,pdfjs} from 'react-pdf/dist/esm/entry.webpack';
 import Controls from './Controls';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -7,35 +7,42 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 function Pdfreader() {
     const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  const[filePath,setfilePath]=useState("");
-  const[browse,setBrowse]=useState("");
-
-  const handlefilePath=(e)=>{
-    console.log(e.target.value);
-    setfilePath(e.target.value);
-  }
-
-  const handleSubmit=(e)=>{
-    setBrowse(filePath);
-  }
+  const[Filepath,setFilePath]=useState();
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
+
+  const handlesubmitfile=(e)=>{
+      const files=e.target.files[0];
+      console.log(e.target.files[0]);
+      const reader=new FileReader();
+      reader.addEventListener('load',()=>{
+          setFilePath(files);
+      })
+      if(files)
+      {
+        reader.readAsDataURL(files)
+      }
+     
+  }
     return (
         <div>
-            <div style={{display:"flex",alignItems:"center",justifyContent:"space-around",flexWrap:"wrap"}}>
-           <input type="text" style={{color:"white"}}  onChange={handlefilePath}></input>
-           <button className="btn btn-small" onClick={handleSubmit}>Browse</button>
+            <br></br>
+            <div style={{display:"flex",alignItems:"center",justifyContent:"space-evenly",flexWrap:"wrap",width:"100%"}}>
+           <input type="file" onChange={handlesubmitfile} accept=".pdf"  style={{width:"25%"}}></input>
            </div>
             <br></br>
-            <Controls/>
+            <Controls numPages={numPages}  pageNumber={pageNumber} setPageNumber={setPageNumber} /><br></br>
+            <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",width:"100%",position:"relative"}}>
             <Document
-            file={browse}
+            file={Filepath}
             onLoadSuccess={onDocumentLoadSuccess}
+            style={{positon:"absolute",width:"100%"}}
             >
-                    <Page pageNumber={pageNumber} />
+            <Page pageNumber={pageNumber} />
             </Document>
+            </div>
 
         </div>
     )
